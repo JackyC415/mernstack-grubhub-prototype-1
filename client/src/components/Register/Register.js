2d1b2aa09450673042c4b526cac2ee74afa6d676
JackyC415 <32407740+JackyC415@users.noreply.github.com>
2019-09-22 20:41:26 -0700

    Implement state changes so user may switch between user and owner registration

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -11,11 +11,15 @@ class Register extends Component {
         this.state = {
             name: '',
             email: '',
-            password: ''
+            password: '',
+            restaurantname: '',
+            zipcode: '',
+            owner: false
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.switchForm = this.switchForm.bind(this);
     }
 
     handleChange = (e) => {
@@ -40,16 +44,33 @@ class Register extends Component {
 
     }
 
+    switchForm = (e) => {
+        (this.state.owner == false) ? this.setState({owner: true}) : this.setState({owner: false});
+    }
+
     render(){
+
+        let ownerForm = null;
+        let accountType = "Owner";
+        if(this.state.owner) {
+            ownerForm = 
+            <div>
+                Restaurant Name: <input type="text" name="restaurantname" placeholder="Restaurant name" value={this.state.restaurantname} onChange={this.handleChange} required></input><br/>
+                ZipCode: <input type="number" name="zipcode" placeholder="Zipcode" value ={this.state.zipcode} onChange={this.handleChange} minLength="6" maxLength="10" required ></input>
+            </div>
+            accountType = "User";
+        }
         return(
             <div class="container">
             <form action="http://127.0.0.1:3001/register" method="post">
-            <h1>Create account</h1>
+            <h1>Create an account</h1>
                     Name: <input type="text" name="name" placeholder="Your name" pattern="^([a-zA-Z]+\s)*[a-zA-Z]+$" value={this.state.name} onChange = {this.handleChange} required></input><br/>
                     Email: <input type="email" name="email" placeholder="example@gmail.com" value={this.state.email} onChange = {this.handleChange} required></input><br/>
                     Password: <input type="password" name="password" placeholder="At least 6 characters" minlength="6" maxlength="16" value={this.state.password} onChange = {this.handleChange} required></input><br/>
-                    <button className="btn btn-primary" onClick={this.state.handleSubmit}>Register</button>
-                    Already have an account? <Link to="/login" className="btn btn-link">Login</Link>
+                    {ownerForm}
+                    <button className="btn btn-primary" onClick={this.handleSubmit}>Register</button>
+                    Already have an account? <Link to="/login" className="btn btn-link">Login</Link><br/>
+                    <a href='#' onClick={this.switchForm}>Sign Up as {accountType}</a><br/>
             </form>
             </div>
         )
